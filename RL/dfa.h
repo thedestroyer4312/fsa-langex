478a367788322d7de23f811f2f70c2ce31535279
thedestroyer4312 <57116739+thedestroyer4312@users.noreply.github.com>
2023-06-14 00:39:51 +0900

    Implement DFA skeleton. Core methods pending

--- RL/dfa.h
+++ RL/dfa.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <algorithm>
 #include <iterator>
 #include <memory>
+#include <stdexcept>
 #include <string_view>
 #include <type_traits>
 #include <unordered_map>
@@ -26,7 +28,13 @@ namespace FSA{
         // If the state or character input is not found, treat as automatic reject
         // q0 - Starting state. q0 in Q
         // F - Subset of Q, tates that are accepting. Encoded in each node
-        std::vector<std::unique_ptr<Node>> states;
+
+        // Note: we choose shared_ptr over unique_ptr because of copy constructors/copy assignment
+        // In that case, no copy of pointers is needed - simply reference already existing ones
+        // This works because transition functions map a subset (or equal to) the `states`
+        // that, with shared_ptr, are guaranteed to exist whereas unique_ptr requires manual deep copying
+        // Thus, it reduces coding complexity and memory usage
+        std::vector<std::shared_ptr<Node>> states;
         std::unordered_map<const Node*, std::unordered_map<char, const Node*>> transitions;
         const Node* start_state;
         
@@ -42,13 +50,19 @@ namespace FSA{
         // Empty DFA recognizes empty string
         DFA();
 
-        bool evaluate(std::stringview s) const;
+        bool evaluate(std::string_view s) const;
+
         template <typename Iterator,
             typename value_type = std::iterator_traits<Iterator>::value_type,
-            std::enable_if_t<std::is_convertible<value_type, char>>::value>
+            std::enable_if_t<std::is_convertible<value_type, char>::value>>
         bool evaluate(Iterator begin, Iterator end) const;
+
+        // Core interface methods for RL_FSA
+        // For detailed descriptions, see RL_FSA class definition (rl_fsa.h)
+        // Union, intersection, concatenation, complement, kleene star
+        
     };
-}
+};
 
 // Method implementations
 namespace FSA{
@@ -56,26 +70,60 @@ namespace FSA{
         config_empty_string();
     }
 
+    void DFA::config_empty_string(){
+        // Initializes a NEW DFA to only accept the empty string
+        // We do this by creating one accept state with no transitions
+        auto state = std::make_shared<Node>(true);
+        
+        // Add to states and set as start state
+        start_state = state.get();
+        states.push_back(state);
+    }
+
     template <typename Iterator,
-        typename value_type = std::iterator_traits<Iterator>::value_type,
-        std::enable_if_t<std::is_convertible<value_type, char>>::value>
+        typename value_type,
+        std::enable_if_t<std::is_convertible<value_type, char>::value>>
     bool DFA::evaluate(Iterator begin, Iterator end) const{
         const Node* curr_node = start_state;
         
         // Trace through DFA
-        while(begin != end){
+        // If string is finished or curr_node is null, break
+        while(begin != end && curr_node){
             char c = *begin; 
             curr_node = delta(curr_node, c);
+            ++begin;
         }
 
-        return curr_node->is_accept;
+        return curr_node && curr_node->is_accept;
     }
 
-    bool DFA::evaluate(std::stringview s) const{
-        return evaluate(s.cbegin(), s.cend());
+    bool DFA::evaluate(std::string_view s) const{
+        // evaluate(s.cbegin(), s.cend());
+        return true;
     }
 
-    const DFA::Node* DFA::delta(Node* n, char c) const{
+    const DFA::Node* DFA::delta(const Node* n, char c) const{
+        // If Node n and character c are both present, return next state
+        // Otherwise, return nullptr
         
+        // Pain in the butt, but we have to do this because const method
+        // cannot use operator[], do manual iterator searching
+        const Node* result = nullptr;
+        
+        // First, check to see if state n has a transition table
+        auto state_transitions_it = transitions.find(n);
+        if(state_transitions_it != transitions.cend()){
+            // Retrieve transition table
+            const auto& state_transitions = state_transitions_it->second;
+
+            // Now, check to see if the transition table for n has an entry for character c
+            auto next_state_it = state_transitions.find(c);
+            if(next_state_it != state_transitions.cend()){
+                // So, there is an appropriate next state. Return it
+                result = next_state_it->second;
+            }
+        }
+
+        return result;
     }
 };
